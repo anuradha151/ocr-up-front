22e6f264a30de40ae1f1d0a3860f9ac701c5f03b
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-01-17 23:37:55 +0530

    Feature : add file uploader and preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React, { useState } from "react";
 
 import NavBar from './/components/NavBar.js';
 
 function App() {
+
+  const [file, setFile] = useState();
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
     <div className="App">
       <NavBar />
+      <br />
+      <input type="file" onChange={handleChange} />
+      <img src={file} />
+
     </div>
   );
 }
