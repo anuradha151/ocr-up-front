942a6264699e05957af6c8c9a69756b8fe5ebb82
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-02-09 09:59:55 +0530

    Fix : alignment issue fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">OCR Uploader</Navbar.Brand>
+        <Navbar.Brand href="#home">OCR Tool</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
