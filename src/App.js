66d62186124775b5b8099f6d0dac65880694a3db
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-02-10 18:05:50 +0530

    Feature : login page added

--- src/App.js
+++ src/App.js
@@ -54,14 +54,12 @@ function App() {
 
 
   return (
-    <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
   );
 }
 
