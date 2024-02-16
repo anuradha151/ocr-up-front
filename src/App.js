8683bd0ce60745aa73d07db08112df6152f1a10b
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-02-16 17:17:13 +0530

    Feature : add page navigation

--- src/App.js
+++ src/App.js
@@ -1,103 +1,20 @@
-import './App.css';
-import React, { useState, useEffect } from "react";
-import NavBar from './/components/NavBar.js';
-import axios from 'axios';
-import { Alert, Button, Card, CardBody, CardText, Col, Container, Image, Row, Spinner } from 'react-bootstrap';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NavBar from "./components/NavBar";
 
 function App() {
-
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [imageText, setImageText] = useState('');
-
-  const [post, setPost] = useState(null);
-
-  const [loading, setLoading] = useState(false);
-
-
-
-  // create sample useEffect to test axios get request using this url https://jsonplaceholder.typicode.com/posts/1
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-      .then((response) => {
-        setPost(response.data.body);
-      })
-      .catch((error) => {
-        console.error('There was an error!', error);
-      });
-  }, []);
-
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setImageText('');
-    try {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
-
-      const response = await axios.post('http://localhost:8000/uploadfile', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        }
-      });
-
-      const textResponse = response.data.text;
-      setImageText(textResponse);
-    } catch (error) {
-      console.error('Error uploading file:', error);
-      setImageText('Upload failed');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const displayData = (data) => {
-    if (typeof data === 'string') {
-      return <Alert variant="success">{data}</Alert>;
-    } else {
-      <p>Cannot read image</p>
-    }
-  };
-
-
   return (
-    <div className="App">
+    <div>
       <NavBar />
       <br />
-      <Container>
-        <Row className="mb-3">
-          <form onSubmit={handleSubmit}>
-            <input type="file" onChange={handleFileChange} />
-            <Button variant="primary" type="submit">Upload</Button>
-          </form>
-        </Row>
-        <Row className="mb-3">
-          {selectedFile &&
-            <div className="image-view">
-              <Image thumbnail src={URL.createObjectURL(selectedFile)} />
-            </div>
-          }
-        </Row>
-        <Row className="justify-content-md-center">
-          <div>
-            {loading &&
-              <Spinner animation="border" role="status" />}
-          </div>
-        </Row>
-        <Row className="mb-3">
-          {imageText && (
-            <Card>
-              <CardBody>
-                <CardText className="mb-2">Image Text</CardText>
-                {displayData(imageText)}
-              </CardBody>
-            </Card>
-          )}
-        </Row>
-      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function About() {
+  return (
+    <Container>
+      <h1>About</h1>
+      <p>
+        This is a simple OCR tool that uses Tesseract.js to extract text from images. 
+      </p>
+      </Container>       
+  )
+}
+
+export default About
