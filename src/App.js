08a446aa6b6a53df8016f5396742510460771928
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-02-16 15:06:47 +0530

    Feature : add loading spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from './/components/NavBar.js';
 import axios from 'axios';
-import { Alert, Button, Card, CardBody, CardText, Container } from 'react-bootstrap';
+import { Alert, Button, Card, CardBody, CardText, Col, Container, Image, Row, Spinner } from 'react-bootstrap';
 
 function App() {
 
   const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState('');
+  const [imageText, setImageText] = useState('');
+
+  const [post, setPost] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+
+
+  // create sample useEffect to test axios get request using this url https://jsonplaceholder.typicode.com/posts/1
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => {
+        setPost(response.data.body);
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
+  }, []);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -15,7 +32,8 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setLoading(true);
+    setImageText('');
     try {
       const formData = new FormData();
       formData.append('file', selectedFile);
@@ -27,25 +45,21 @@ function App() {
       });
 
       const textResponse = response.data.text;
-      setUploadStatus(textResponse);
+      setImageText(textResponse);
     } catch (error) {
       console.error('Error uploading file:', error);
-      setUploadStatus('Upload failed');
+      setImageText('Upload failed');
+    } finally {
+      setLoading(false);
     }
   };
 
   const displayData = (data) => {
-    // Example with simple text data
     if (typeof data === 'string') {
       return <Alert variant="success">{data}</Alert>;
+    } else {
+      <p>Cannot read image</p>
     }
-
-    // Adapt this logic to handle your specific data structure
-    // You can use Bootstrap components like Table, ListGroup, etc.
-
-    return (
-      <p>Please implement data display logic based on your API response structure.</p>
-    );
   };
 
 
@@ -54,24 +68,35 @@ function App() {
       <NavBar />
       <br />
       <Container>
-        <form onSubmit={handleSubmit}>
-          <input type="file" onChange={handleFileChange} />
-          <Button variant="primary" type="submit">Upload</Button>
-        </form>
-        <br />
-        {selectedFile &&
-          <img src={URL.createObjectURL(selectedFile)} />
-        }
-        <br />
-        {uploadStatus && (
-          <Card>
-            <CardBody>
-              <CardText className="mb-2">Image Text</CardText>
-              {/* Display data using appropriate Bootstrap components */}
-              {displayData(uploadStatus)}
-            </CardBody>
-          </Card>
-        )}
+        <Row className="mb-3">
+          <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileChange} />
+            <Button variant="primary" type="submit">Upload</Button>
+          </form>
+        </Row>
+        <Row className="mb-3">
+          {selectedFile &&
+            <div className="image-view">
+              <Image thumbnail src={URL.createObjectURL(selectedFile)} />
+            </div>
+          }
+        </Row>
+        <Row className="justify-content-md-center">
+          <div>
+            {loading &&
+              <Spinner animation="border" role="status" />}
+          </div>
+        </Row>
+        <Row className="mb-3">
+          {imageText && (
+            <Card>
+              <CardBody>
+                <CardText className="mb-2">Image Text</CardText>
+                {displayData(imageText)}
+              </CardBody>
+            </Card>
+          )}
+        </Row>
       </Container>
     </div>
   );
